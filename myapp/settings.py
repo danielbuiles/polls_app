4de6d942091bf0a3c569c56fce857e859193b13e
Daniel Builes <danielbgar@gmail.com>
2021-11-30 01:05:31 +0800

    migrations and models in the app polls

--- myapp/settings.py
+++ myapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'GMT-5'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
 
+admin.site.register(Question)
+admin.site.site_header = 'Pollster Admin'
+admin.site.site_title = 'Pollster Admin Area'
+admin.site.index_title = 'Welcome to the Pollster Admin Area'
 # Register your models here.
