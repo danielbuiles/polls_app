bcb308d457af700ca19368022c049b819c3c2720
Daniel Builes <danielbgar@gmail.com>
2021-11-30 02:59:02 +0800

    se crearon mas vistas y configuracion avanzada de rutas

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.details, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+app_name = 'polls'
