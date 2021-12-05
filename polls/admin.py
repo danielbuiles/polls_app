b0efff81b405f2ab1abe49030bbfa62450e4fafe
Daniel Builes <danielbgar@gmail.com>
2021-12-05 11:01:36 +0800

    nuevas vistas y codigo mas limpio

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 
+admin.site.register(Choice)
 admin.site.register(Question)
 admin.site.site_header = 'Pollster Admin'
 admin.site.site_title = 'Pollster Admin Area'
